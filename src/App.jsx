43b905d7e2ee93e786a3c40a36730b6d2e1f80cb
marlon <marlonsuyuc@gmail.com>
2023-08-27 18:50:33 -0600

    add custom hook for search

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,48 @@
-import { useState } from 'react'
+import { useState, useRef, useEffect } from 'react'
 import Buttons from './components/Buttons'
 import Pokemon from './components/Pokemon'
 import { usePokeApi } from './hooks/usePokeApi'
 import { Loader } from './components/Loader'
+import Formulario from './components/Formulario'
+
+function useSearch() {
+  const [search, updateSearch] = useState('')
+  const [error, setError] = useState(null)
+  const isFirstInput = useRef('')
+
+  useEffect(() => {
+    if (isFirstInput.current) {
+      const value = (isFirstInput.current = search)
+      console.log(value)
+      console.log(search)
+      isFirstInput.current = search === ''
+      return
+    }
+
+    if (search === '') {
+      setError('No se puede buscar una película vacía')
+      return
+    }
+
+    if (search.match(/^\d+$/)) {
+      setError('No se puede buscar una película con un número')
+      return
+    }
+
+    if (search.length < 3) {
+      setError('La búsqueda debe tener al menos 3 caracteres')
+      return
+    }
+
+    setError(null)
+  }, [search])
+
+  return { search, updateSearch, error }
+}
 
 function App() {
+  const { search, updateSearch, error } = useSearch()
+
   const {
     prevUrl,
     pokemons,
@@ -14,59 +52,29 @@ function App() {
     pokemonSearch,
     singlePokemon,
     setpokemonSearch,
-  } = usePokeApi()
-  const [search, setSearch] = useState('')
+  } = usePokeApi({ search })
 
-  const handleSubmit = (e) => {
-    e.preventDefault()
+  const handleSubmit = (event) => {
+    event.preventDefault()
     singlePokemon({ search })
   }
 
-  const handleChange = (e) => {
-    const query = e.target.value.toLowerCase()
-    setSearch(query)
+  const handleChange = (event) => {
+    const newSearch = event.target.value.toLowerCase()
+    updateSearch(newSearch)
     setpokemonSearch([])
   }
+
   return (
-    <div className='block w-full max-w-6xl py-4 px-10 md:px-20 pb-32 m-auto'>
-      <h1 className='mt-3 text-center text-5xl font-bold'>Poké Api</h1>
-
-      <form onSubmit={handleSubmit} className='my-8 '>
-        <div className='relative'>
-          <div className='absolute inset-y-0 left-0 flex items-center pl-3 pointer-events-none'>
-            <svg
-              className='w-4 h-4 text-gray-500 dark:text-gray-400'
-              aria-hidden='true'
-              xmlns='http://www.w3.org/2000/svg'
-              fill='none'
-              viewBox='0 0 20 20'
-            >
-              <path
-                stroke='currentColor'
-                strokeLinecap='round'
-                strokeLinejoin='round'
-                strokeWidth='2'
-                d='m19 19-4-4m0-7A7 7 0 1 1 1 8a7 7 0 0 1 14 0Z'
-              />
-            </svg>
-          </div>
-          <input
-            autoFocus
-            onChange={handleChange}
-            type='search'
-            id='default-search'
-            className='block w-full p-4 pl-10 text-sm text-gray-900 border border-gray-300 rounded-lg bg-gray-50 focus:ring-blue-500 focus:border-blue-500 outline-none'
-            placeholder='Buscar pokemon'
-          />
-          <button
-            type='submit'
-            className='text-white absolute right-2.5 bottom-2.5 bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-4 py-2 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800'
-          >
-            Search
-          </button>
-        </div>
-      </form>
-
+    <div className='block w-full max-w-6xl py-4 px-10 md:px-20 pb-32 m-auto bg-gradient-to-r from-cyan-500 via-blue-800  to-cyan-400 min-h-screen'>
+      <h1 className='mt-3 text-center text-5xl lg:text-6xl font-bold text-white/40'>
+        Poké Api
+      </h1>
+      <Formulario
+        search={search}
+        handleChange={handleChange}
+        handleSubmit={handleSubmit}
+      />
       <section>
         {!search && (
           <div className='ml-auto mr-auto mt-8 mb-4'>
